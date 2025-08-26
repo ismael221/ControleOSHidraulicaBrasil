ec553ccaa282ddfce1b00bfdce97f3a4b0b719e0
Ismael Nunes <ismaeldenunes@gmail.com>
2025-08-26 09:28:42 -0300

    feat - Eventos de gerar e botão de ação de alterar status OS

---
+++ src/br/com/bolt/controle/os/view/AcaoAlterarStatusOS.java
@@ -0,0 +1,12 @@
+package br.com.bolt.controle.os.view;
+
+import br.com.sankhya.extensions.actionbutton.AcaoRotinaJava;
+import br.com.sankhya.extensions.actionbutton.ContextoAcao;
+
+public class AcaoAlterarStatusOS implements AcaoRotinaJava {
+
+    @Override
+    public void doAction(ContextoAcao contexto) throws Exception {
+
+    }
+}
---
+++ src/br/com/bolt/controle/os/view/EventGerarOs.java
@@ -0,0 +1,43 @@
+package br.com.bolt.controle.os.view;
+
+import br.com.sankhya.extensions.eventoprogramavel.EventoProgramavelJava;
+import br.com.sankhya.jape.event.PersistenceEvent;
+import br.com.sankhya.jape.event.TransactionContext;
+
+public class EventGerarOs implements EventoProgramavelJava {
+
+    @Override
+    public void beforeInsert(PersistenceEvent event) throws Exception {
+
+    }
+
+    @Override
+    public void beforeUpdate(PersistenceEvent event) throws Exception {
+
+    }
+
+    @Override
+    public void beforeDelete(PersistenceEvent event) throws Exception {
+
+    }
+
+    @Override
+    public void afterInsert(PersistenceEvent event) throws Exception {
+
+    }
+
+    @Override
+    public void afterUpdate(PersistenceEvent event) throws Exception {
+
+    }
+
+    @Override
+    public void afterDelete(PersistenceEvent event) throws Exception {
+
+    }
+
+    @Override
+    public void beforeCommit(TransactionContext tranCtx) throws Exception {
+
+    }
+}
