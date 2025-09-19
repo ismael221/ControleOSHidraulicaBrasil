99d7796686aef49b219bd4f36a7c5278e9961d87
Ismael Rodrigues <ismael.campos@boltconsultoria.com.br>
2025-09-19 15:22:26 -0300

    feat - Mapamento e criação de metodo para salvamento dos historicos de movimento da OS

---
+++ src/br/com/bolt/controle/os/model/HistoricoMovimento.java
@@ -0,0 +1,72 @@
+package br.com.bolt.controle.os.model;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+public class HistoricoMovimento {
+
+    private BigDecimal idOs;
+    private Timestamp dtNeg;
+    private String tipMov;
+    private BigDecimal codTipoOper;
+    private Double vlrNota;
+
+    public HistoricoMovimento(BigDecimal idOs, Timestamp dtNeg, String tipMov, BigDecimal codTipoOper, Double vlrNota) {
+        this.idOs = idOs;
+        this.dtNeg = dtNeg;
+        this.tipMov = tipMov;
+        this.codTipoOper = codTipoOper;
+        this.vlrNota = vlrNota;
+    }
+
+    public BigDecimal getIdOs() {
+        return idOs;
+    }
+
+    public void setIdOs(BigDecimal idOs) {
+        this.idOs = idOs;
+    }
+
+    public Timestamp getDtNeg() {
+        return dtNeg;
+    }
+
+    public void setDtNeg(Timestamp dtNeg) {
+        this.dtNeg = dtNeg;
+    }
+
+    public String getTipMov() {
+        return tipMov;
+    }
+
+    public void setTipMov(String tipMov) {
+        this.tipMov = tipMov;
+    }
+
+    public BigDecimal getCodTipoOper() {
+        return codTipoOper;
+    }
+
+    public void setCodTipoOper(BigDecimal codTipoOper) {
+        this.codTipoOper = codTipoOper;
+    }
+
+    public Double getVlrNota() {
+        return vlrNota;
+    }
+
+    public void setVlrNota(Double vlrNota) {
+        this.vlrNota = vlrNota;
+    }
+
+    @Override
+    public String toString() {
+        return "HistoricoMovimento{" +
+                "idOs=" + idOs +
+                ", dtNeg=" + dtNeg +
+                ", tipMov='" + tipMov + '\'' +
+                ", codTipoOper=" + codTipoOper +
+                ", vlrNota=" + vlrNota +
+                '}';
+    }
+}
